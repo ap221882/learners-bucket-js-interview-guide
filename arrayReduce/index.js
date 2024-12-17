332f0e768428170187f045e09cca1c68f15a6745
ajay <ap221882@gmail.com>
2024-12-18 01:06:15 +0530

    asyncSeriesExecuter

--- arrayReduce/index.js
+++ arrayReduce/index.js
@@ -9,7 +9,7 @@
 
 // sum of all values
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-const sum = (arr)=>arr.reduce((prevValue, currValue, currIndex, array)=>{
+const sum = (arr) => arr.reduce((prevValue, currValue, currIndex, array) => {
   const nextValue = prevValue + currValue;
   return nextValue;
 }, 0);
@@ -22,14 +22,14 @@ console.log('====================================');
 // segregation of all values
 const arr2 = [1.1, 1.2, 1.3, 2.2, 2.3, 2.4];
 
-const segregate = (arr) => arr.reduce((prev, curr, currIdx, arr)=>{
+const segregate = (arr) => arr.reduce((prev, curr, currIdx, arr) => {
   const florredValue = Math.floor(curr);
-  if(!prev[florredValue]){
+  if (!prev[florredValue]) {
     prev[florredValue] = []
   };
   prev[florredValue].push(curr);
   return prev;
-},{})
+}, {})
 
 console.log('====================================');
 console.log(segregate(arr2));
@@ -38,18 +38,18 @@ console.log('====================================');
 // piping pure fns
 
 const upperCase = (str) => {
-return str.toUpperCase();
+  return str.toUpperCase();
 };
 const reverse = (str) => {
-return str.split('').reverse().join('');
+  return str.split('').reverse().join('');
 };
 const append = (str) => {
-return "Hello " + str;
+  return "Hello " + str;
 };
 
 const arr3 = [upperCase, reverse, append];
 
-const pipe = (arr, valueToBePassed) => arr.reduce((prevValue, currValue, currentIndex, _array)=>{
+const pipe = (arr, valueToBePassed) => arr.reduce((prevValue, currValue, currentIndex, _array) => {
   const nextValue = currValue(prevValue);
   return nextValue;
 }, valueToBePassed)
@@ -57,3 +57,27 @@ const pipe = (arr, valueToBePassed) => arr.reduce((prevValue, currValue, current
 console.log('====================================');
 console.log(pipe(arr3, 'learnersbucket'));
 console.log('====================================');
+
+// Async tasks executor
+
+const asyncTask = (time) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(`Completing ${time}`);
+    }, time * 1000)
+  })
+}
+
+const promises = [
+  asyncTask(3),
+  asyncTask(1),
+  asyncTask(7),
+  asyncTask(2),
+  asyncTask(5),
+  ];
+
+const asyncSeriesExecuter = (promiseArray) => promiseArray.reduce((prevValue, currentValue, currentIndex, _array) => {
+  return prevValue.then(() => currentValue.then((val) => console.log(val)))
+}, Promise.resolve());
+
+asyncSeriesExecuter(promises);
